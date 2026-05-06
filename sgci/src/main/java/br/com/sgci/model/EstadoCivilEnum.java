910c21e39b4e8b48cb281d32909ed2989aa34369
Vinicius Anderle <vinianderle00@gmail.com>
2026-05-06 20:51:02 -0300

    feat: inclui mapeamento de novas entidades

---
+++ sgci/src/main/java/br/com/sgci/model/EstadoCivilEnum.java
@@ -0,0 +1,6 @@
+package br.com.sgci.model;
+
+public enum EstadoCivilEnum {
+
+	PESSOA_FISICA, PESSOA_JURIDICA
+}
---
+++ sgci/src/main/java/br/com/sgci/model/TipoPessoaEnum.java
@@ -0,0 +1,6 @@
+package br.com.sgci.model;
+
+public enum TipoPessoaEnum {
+
+	CASADO, SOLTEIRO, DIVORCIADO
+}
